b9e5f224a9fefaeb06ae8fb01f9e17cb23f1bdda
Richard Sawey <richard.sawey@zuora.com>
2013-07-24 13:00:16 -0700

    completed basic profiler

--- profile.sh
+++ profile.sh
@@ -18,7 +18,7 @@ COLIST=`head -1 $CSVFILE`
 ROWCOUNT=`sqlite3 $DB "select count(*) from $TABLE;"`
 echo $ROWCOUNT
 sqlite3 $DB "drop table profile;"
-sqlite3 $DB "create table profile(column_name, min_value, max_value, num_nulls, num_values, data_type, num_rows, pct_null, pct_values);"
+sqlite3 $DB "create table profile(column_name, min_value, max_value, num_nulls, pct_null, num_values, num_rows);"
 
 
 # head -1 $CSVFILE
@@ -31,7 +31,9 @@ do
     MIN=`sqlite3 $DB "select min([$COL]) from $TABLE;"`
     MAX=`sqlite3 $DB "select max([$COL]) from $TABLE;"`
     NULLCOUNT=`sqlite3 $DB "select $ROWCOUNT - count([$COL]) from $TABLE;"`
-    sqlite3 $DB "insert into profile(column_name, min_value, max_value, num_nulls, num_rows) values( '$COL', '$MIN', '$MAX', $NULLCOUNT, $ROWCOUNT);"
+    NULLPCT=`sqlite3 $DB "select round((round(count(*),2) - round(count([$COL]),2))/$ROWCOUNT*100,2) from $TABLE;"`
+    VALUECOUNT=`sqlite3 $DB "select count(distinct [$COL]) from $TABLE;"`
+    sqlite3 $DB "insert into profile(column_name, min_value, max_value, num_nulls, num_rows, pct_null, num_values) values( '$COL', '$MIN', '$MAX', $NULLCOUNT, $ROWCOUNT, $NULLPCT, $VALUECOUNT);"
 done
 unset IFS
 sqlite3 $DB "select * from profile;"
